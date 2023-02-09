2798c8549b0e2dabc834b2cf0e77163967e3da4d
NotCuteButNew <2386884119@qq.com>
2023-02-09 15:17:30 +0800

    编写并运行成功

---
+++ rosenblatt.py
@@ -0,0 +1,23 @@
+﻿import numpy as np
+from matplotlib import pyplot as plt
+
+virulence = np.random.rand(100)
+volume = np.random.rand(100)
+virulence.sort()
+volume.sort()
+
+plt.scatter(volume, virulence)
+
+w = 0
+alpha = 0.05
+
+for m in range(10000):
+    for i in range(100):
+        x = volume[i]
+        y = virulence[i]
+        predition = y * w
+        error = y-predition
+        w = w + alpha * error * x
+
+plt.plot(volume, volume*w)
+plt.show()
