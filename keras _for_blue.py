578190657c6ef484ff44c4cc8105b733c7279c5d
NotCuteButNew <2386884119@qq.com>
2023-02-28 09:53:33 +0800

    毒性仅和体积大小有关

--- keras _for_blue.py
+++ keras _for_blue.py
@@ -1,1 +1,29 @@
-﻿
+﻿import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+
+m = 100
+y = np.random.rand(m)
+x = np.random.rand(m)
+y.sort()
+x.sort()
+for i in range(m):
+    if y[i] < 0.6:
+        y[i] = 0
+    else:
+        y[i] = 1
+w = 0
+b = 0
+plt.scatter(x, y)
+
+model = Sequential()
+dense = Dense(units=1, activation='sigmoid', input_dim=1)
+model.add(dense)
+
+model.compile(loss='mean_squared_error', optimizer='sgd', metrics=['accuracy'])
+model.fit(x, y, epochs=5000, batch_size=10)
+
+pres = model(x)
+plt.plot(x, pres)
+plt.show()
